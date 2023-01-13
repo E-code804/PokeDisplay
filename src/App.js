c8c282181666d417370f52ed6f41803be495952b
Erik Pfeffer <erikpfeffer@Eriks-Air.fios-router.home>
2023-01-12 21:35:06 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Input from "./components/Input";
+import "./styles.css";
+
+// TODO error outputs for when name DNE, already in array, limit reached
+const MAX_PARTY_SIZE = 6;
 
 function App() {
+  const [name, setName] = useState("");
+  const [pokeNames, setPokeNames] = useState([]);
+
+  const handleClick = () => {
+    if (!pokeNames.includes(name) && pokeNames.length < MAX_PARTY_SIZE) {
+      setPokeNames([...pokeNames, name]);
+    }
+    setName("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-div">
+      <h1 className="mt-5">PokeTeam Display</h1>
+      <p>Pick your favorite Pokemon</p>
+
+      <Input setName={setName} handleClick={handleClick} name={name} />
+      <ul>
+        {pokeNames.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+
+      <div className="pokemon-display mt-3 mb-5">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div className="pokemon"></div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div className="pokemon"></div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div className="pokemon"></div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div className="pokemon"></div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div className="pokemon"></div>
+            </div>
+            <div className="col-lg-4 col-md-6 col-sm-12">
+              <div className="pokemon"></div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
